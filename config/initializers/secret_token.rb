47cba1fda99873e4c7c2e40ba7644ec46c4a2e8d
Andrew Benson <andebenson@gmail.com>
2013-06-24 14:28:58 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VocabBuilder::Application.config.secret_token = 'c0bd8eb1f78353f09a8f76b6ae603ecbf0f43d1a22fa3ec783fc048303799f32ab12aa3201b8dd26c9ded2b6e65dba38917145654832aabdb46f579184ee3f36'
